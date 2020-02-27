0cd7ac55b3c92863732255407f885e556e73e889
auahme21 <auahme21@shs-m104lab-02.shrewsbury.k12.ma.us>
2020-02-27 14:00:54 -0500

    Update hangMan.py

--- hangMan.py
+++ hangMan.py
@@ -2,7 +2,27 @@ lives = 6
 word = ""
 letter = ""
 updatedSpaces= []
-
+global UpdateSpaces
+global lives
+global word
+letter =["h"]
+word = ['hamza']
+def check(UpdateSpaces):
+    lives = 6
+    UpdateSpaces= ['-','-','-','-','-']
+    if letter in word:
+        word.find(letter,0,len(word)-1)
+        UpdateSpaces[0]='h'
+        word.find(letter,1,len(word)-1)
+        UpdateSpaces[1]='a'
+        word.find(letter,2,len(word)-1)
+        UpdateSpaces[2]='m'
+        word.find(letter,3,len(word)-1)
+        UpdateSpaces[3]='z'
+        word.find(letter,4,len(word)-1)
+        UpdateSpaces[4]='a'
+    else:
+        lives = lives - 1
 def initialize():
     global word
     global updatedSpaces
